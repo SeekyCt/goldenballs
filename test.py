0fa94a1667072f2520d183ebd3bb530ba832613d
Seeky <58006653+SeekyCt@users.noreply.github.com>
2023-05-25 18:36:26 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+from goldenballs.game import Player, Game
+
+if __name__ == '__main__':
+    game = Game()
+    for player in [Player(f"Player {i}") for i in range(4)]:
+        print(game.on_join(player))
+
+    # Round 1
+    print(game.on_vote(game.players[0], game.players[1]))
+    for player in game.players[1:]:
+        print(game.on_vote(player, game.players[0]))
+    while msg := game.get_channel_message():
+        print(msg)
+
+    # Round 2
+    print(game.on_vote(game.players[0], game.players[1]))
+    for player in game.players[1:]:
+        print(game.on_vote(player, game.players[0]))
+    while msg := game.get_channel_message():
+        print(msg)
+    
+    # Round 3
+    for i in range(11):
+        player = game.players[(i // 2) % 2]
+        print(game.on_pick(player, 1))
+    while msg := game.get_channel_message():
+        print(msg)
+    
+    # Round 4
+    for player in game.players:
+        game.on_split(player)
+    while msg := game.get_channel_message():
+        print(msg)
+    
+    print(game.get_results())
+    assert game.is_finished()
+    
